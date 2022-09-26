cd882289cf704b57b5034bf2bdfe61b96148c4c6
Roberto Baiochi <107875368+RobertoBaiochi@users.noreply.github.com>
2022-09-26 03:43:00 -0300

    alteracao target-blank footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.scss'
 function Footer() {
     return ( 
         <footer className="attribution">
-            <p>Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"> Frontend Mentor</a>.
+            <p>Challenge by <a href="https://www.frontendmentor.io?ref=challenge"> Frontend Mentor</a>.
             Coded by <a href="https://www.linkedin.com/in/roberto-baiochi/">Roberto Baiochi</a>.</p>
       </footer>
      );
