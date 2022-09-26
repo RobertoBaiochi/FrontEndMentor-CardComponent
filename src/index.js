b7a9e7e35ad2e8845e2fd621888cf1657ab289c2
Roberto Baiochi <107875368+RobertoBaiochi@users.noreply.github.com>
2022-09-26 03:34:35 -0300

    reestruturando index-js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './main/App'
-import 'font-awesome/css/font-awesome.min.css'
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
